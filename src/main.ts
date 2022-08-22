a6274a171b1d692273b198c003c083fddaed6aa4
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2022-08-23 01:10:30 +0900

    Fix example according to current driver impl

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,43 @@
 import Timer from 'timer'
 import SCServo from 'scservo'
 declare const button: {
-  a: {
+  [key: string]: {
     onChanged: (this: { read: () => number }) => void
   }
 }
 
 const servo = new SCServo({
-  id: 1
+  id: 1,
 })
 
 let torqueEnabled = true
 let angle = 0
-let tick = 30
-Timer.repeat(() => {
+let tick = 10
+Timer.repeat(async () => {
   if (!torqueEnabled) {
     return
   }
   angle += tick
-  trace(`writing angle: ${angle}\n`)
-  if (angle >= 1000 || angle < 30) {
+  trace(`${servo.id}...writing angle: ${angle}\n`)
+  if (angle >= 200 || angle <= 0) {
     tick = -tick
   }
-  servo.setAngleInTime(angle, 500)
+  await servo.setAngleInTime(angle, 500)
 }, 1000)
 Timer.repeat(async () => {
-  const angle = await servo.readStatus()
-  trace(`current angle: ${angle}\n`)
+  const { angle } = await servo.readStatus()
+  trace(`${servo.id}...current angle: ${angle}\n`)
 }, 33)
 
-button.a.onChanged = function() {
+button.a.onChanged = function () {
   if (!this.read()) {
     torqueEnabled = !torqueEnabled
     servo.setTorque(torqueEnabled)
+  }
+}
+
+button.b.onChanged = function () {
+  if (!this.read() && servo.id !== 2) {
+    servo.flashId(2)
   }
 }
